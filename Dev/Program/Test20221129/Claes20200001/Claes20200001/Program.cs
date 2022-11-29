d25a1207fbd9cdfa2e5a585f0c6bf9845f4c9139
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-30 00:14:01 +0900

    Voyager 1 is 23684111862.653 kilometers away from the Sun.

--- Dev/Program/Test20221129/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test20221129/Claes20200001/Claes20200001/Program.cs
@@ -54,13 +54,9 @@ namespace Charlotte
 		{
 			// -- choose one --
 
-			//new Test0001().Test01();
+			new Test0001().Test01();
 			//new Test0002().Test01();
 			//new Test0003().Test01();
-			//new Test0003().Test02();
-			//new Test0004().Test01();
-			//new Test0004().Test02();
-			new Test0005().Test01();
 
 			// --
 		}
--- Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
+using Charlotte.Commons;
 
 namespace Charlotte.Tests
 {
-	public class Test0005
+	public class Test0001
 	{
 		public void Test01()
 		{
-			Console.WriteLine("Hello, Happy World!");
+			// none
 		}
 	}
 }
