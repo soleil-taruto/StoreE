fa850f8b224caebe0993dd50962e388513822492
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-30 15:46:13 +0900

    Voyager 1 is 23685056657.229 kilometers away from the Sun.

--- Dev/Program/Wait/Petra20200001/Program.c
+++ Dev/Program/Wait/Petra20200001/Program.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#define error() \
+	{ printf("ERROR %d\n", __LINE__); exit(2); }
+
+#define errorCase(status) \
+	{ if ((status)) error(); }
+
+#define GetMin(a, b) ((a) < (b) ? (a) : (b))
+#define GetMax(a, b) ((a) < (b) ? (b) : (a))
+
+#define WAIT_SEC_MAX 999999
 
 main()
 {
-	// none
+	int waitSec;
+	int millis;
+	int lastPrintRemSec = -1;
+
+	errorCase(__argc != 2);
+	waitSec = atoi(__argv[1]);
+	errorCase(waitSec < 0 || WAIT_SEC_MAX < waitSec);
+	millis = waitSec * 1000;
+
+	for (; ; )
+	{
+		if (lastPrintRemSec != millis / 1000)
+		{
+			lastPrintRemSec = millis / 1000;
+			printf("\r%d 秒待っています。中止するには ESC 続行するには ENTER を押して下さい ... ", lastPrintRemSec);
+		}
+
+		while (_kbhit())
+		{
+			int key = _getch();
+
+			if (key == 0x1b) // ? ESC
+			{
+				printf("\n中止します。\n");
+				exit(1);
+			}
+			if (key == 0x0d) // ? ENTER
+			{
+				printf("\n続行します。\n");
+				exit(0);
+			}
+
+			if (key == '+') // プラス 1 分
+			{
+				millis += 60000;
+			}
+			else if (key == '-') // マイナス 1 分
+			{
+				millis -= 60000;
+			}
+			else if (key == '*') // プラス 1 時間
+			{
+				millis += 3600000;
+			}
+			else if (key == '/') // 残り時間クリア
+			{
+				millis = 0;
+			}
+
+			millis = GetMin(millis, WAIT_SEC_MAX * 1000);
+			millis = GetMax(millis, 3000);
+		}
+
+		if (millis <= 0)
+		{
+			printf("\nタイムアウトしました。\n");
+			exit(0);
+		}
+
+		Sleep(250);
+		millis -= 250;
+	}
 }
