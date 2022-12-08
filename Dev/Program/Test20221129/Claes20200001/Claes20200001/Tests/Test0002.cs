ab19ff888af1880ad4acb5bee87416a1a054b8b9
soleil.taruto <soleil.taruto@gmail.com>
2022-12-09 04:56:58 +0900

    Voyager 1 is 23697533478.535 kilometers away from the Sun.

--- Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0002.cs
+++ Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -13,12 +13,9 @@ namespace Charlotte.Tests
 
 		public void Test01()
 		{
-			//Test01_a(@"C:\Factory", file => Path.GetExtension(file).ToLower() == ".c", SCommon.ENCODING_SJIS);
-			//Test01_a(@"C:\Factory", file => Path.GetExtension(file).ToLower() == ".h", SCommon.ENCODING_SJIS);
-			//Test01_a(@"C:\Dev", file => Path.GetExtension(file).ToLower() == ".cs" && !file.ToLower().EndsWith(".designer.cs"), Encoding.UTF8);
-			Test01_a(@"C:\develop", file => Path.GetExtension(file).ToLower() == ".cs", Encoding.UTF8);
-			Test01_a(@"C:\develop", file => Path.GetExtension(file).ToLower() == ".cpp", Encoding.UTF8);
-			Test01_a(@"C:\develop", file => Path.GetExtension(file).ToLower() == ".vb", Encoding.UTF8);
+			Test01_a(@"C:\Factory", file => Path.GetExtension(file).ToLower() == ".c", SCommon.ENCODING_SJIS);
+			Test01_a(@"C:\Factory", file => Path.GetExtension(file).ToLower() == ".h", SCommon.ENCODING_SJIS);
+			Test01_a(@"C:\Dev", file => Path.GetExtension(file).ToLower() == ".cs" && !file.ToLower().EndsWith(".designer.cs"), Encoding.UTF8);
 
 			// ====
 
@@ -51,7 +48,7 @@ namespace Charlotte.Tests
 
 						if (c == 1)
 						{
-							//FoundFiles.Add(file);
+							FoundFiles.Add(file);
 						}
 					}
 				}
