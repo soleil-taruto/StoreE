6d757fd86500ccbc08ff072e610163225c7dea11
soleil.taruto <soleil.taruto@gmail.com>
2022-12-09 04:30:25 +0900

    Voyager 1 is 23697506587.337 kilometers away from the Sun.

--- Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0002.cs
+++ Dev/Program/Test20221129/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Charlotte.Commons;
 
 namespace Charlotte.Tests
 {
@@ -9,7 +11,45 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Test01_a(@"C:\Factory", "*.c", SCommon.ENCODING_SJIS);
+			Test01_a(@"C:\Factory", "*.h", SCommon.ENCODING_SJIS);
+			Test01_a(@"C:\Dev", "*.cs", Encoding.UTF8);
+		}
+
+		private void Test01_a(string rootDir, string wildCard, Encoding encoding)
+		{
+			string[] files = Directory.GetFiles(rootDir, wildCard, SearchOption.AllDirectories);
+
+			foreach (string file in files)
+			{
+				string[] lines = File.ReadAllLines(file, encoding);
+
+				for (int index = 1; index + 1 < lines.Length; index++)
+				{
+					string trLine_01 = lines[index - 1].Trim();
+					string trLine_02 = lines[index].Trim();
+					string trLine_03 = lines[index + 1].Trim();
+
+					if (trLine_02.StartsWith("else"))
+					{
+						int c = 0;
+
+						if (trLine_01.StartsWith("}")) c++;
+						if (trLine_03.StartsWith("{")) c++;
+
+						if (c == 1)
+						{
+							Console.WriteLine(file);
+						}
+					}
+				}
+
+				/*
+				foreach (string line in lines)
+					if (line.EndsWith("\t") || line.EndsWith(" "))
+						Console.WriteLine(file);
+				 * */
+			}
 		}
 	}
 }
