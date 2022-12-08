cc95199426894415a887b3a70049d66a6fe0c952
soleil.taruto <soleil.taruto@gmail.com>
2022-12-09 02:04:17 +0900

    Voyager 1 is 23697358466.160 kilometers away from the Sun.

--- Dev/Program/00_Utilities/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/00_Utilities/Claes20200001/Claes20200001/Program.cs
@@ -54,7 +54,7 @@ namespace Charlotte
 		{
 			// -- choose one --
 
-			//new Test0001().Test01(); // AESCipher
+			new Test0001().Test01(); // AESCipher
 			//new Test0002().Test01(); // RingCipher
 			//new Test0002().Test02(); // RingCipher
 			//new Test0003().Test01(); // RingCipherFile
@@ -69,7 +69,7 @@ namespace Charlotte
 			//new Test0008().Test01(); // StringSpliceSequencer
 			//new Test0008().Test02(); // StringSpliceSequencer
 			//new Test0009().Test01(); // Adler32
-			new Test0010().Test01(); // MillerRabin
+			//new Test0010().Test01(); // MillerRabin
 			//new Test0010().Test02(); // MillerRabin
 			//new Test0011().Test01(); // JsonNode
 			//new Test0012().Test01(); // XMLNode
