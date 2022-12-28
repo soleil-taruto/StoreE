0ec671892c586f2f928f662cabf0d9d0fa2648d5
soleil.taruto <soleil.taruto@gmail.com>
2022-12-29 08:57:16 +0900

    Voyager 1 is 23726965225.401 kilometers away from the Sun.

--- Dev/GameJS/SevenBridge/Gattonero20200001/Tests/Test0001.js
+++ Dev/GameJS/SevenBridge/Gattonero20200001/Tests/Test0001.js
@@ -4,5 +4,26 @@
 
 function* <generatorForTask> Test01()
 {
-	// none
+	var<int[]> arr = [ 100, 200, 300 ];
+
+	console.log(arr);
+
+	var<string> str = JoinString(arr);
+
+	console.log(str);
+}
+
+function* <generatorForTask> Test02()
+{
+	var<string[]> arr = [ "1", "2", "3", "4", "5" ];
+
+	console.log(arr);
+
+	arr = Select(arr, v => ParseInt(v));
+
+	console.log(arr);
+
+	arr = Where(arr, v => v % 2 != 0);
+
+	console.log(arr);
 }
