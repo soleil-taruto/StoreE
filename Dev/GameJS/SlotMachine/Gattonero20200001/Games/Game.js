989841363f6271e545b4f1f545b24df0436d58c8
soleil.taruto <soleil.taruto@gmail.com>
2022-12-29 09:18:12 +0900

    Voyager 1 is 23726986542.211 kilometers away from the Sun.

---
+++ Dev/GameJS/SlotMachine/Gattonero20200001/Games/Game.js
@@ -0,0 +1,31 @@
+/*
+	ゲーム・メイン
+*/
+
+// カメラ位置(整数)
+var<D2Point_t> Camera = CreateD2Point(0.0, 0.0);
+
+// ゲーム用タスク
+var<TaskManager_t> GameTasks = CreateTaskManager();
+
+var<int> @@_Credit = 1000;
+
+function <void> AddGameCredit(<int> value)
+{
+	@@_Credit += value;
+}
+
+function* <generatorForTask> GameMain()
+{
+	FreezeInput();
+	ClearAllActor();
+	ClearAllTask(GameTasks);
+
+	for (; ; )
+	{
+		error(); // TODO
+	}
+	FreezeInput();
+	ClearAllActor();
+	ClearAllTask(GameTasks);
+}
