6a903664e57b26070fab909379b2f3d685937335
soleil.taruto <soleil.taruto@gmail.com>
2022-12-31 23:06:33 +0900

    Voyager 1 is 23730744852.817 kilometers away from the Sun.

--- Dev/Program/RemoteCmdServer/Claes20200001/Claes20200001/Commons/SCommon.cs
+++ Dev/Program/RemoteCmdServer/Claes20200001/Claes20200001/Commons/SCommon.cs
@@ -353,6 +353,10 @@ namespace Charlotte.Commons
 			return new HashSet<string>(new IECompStringIgnoreCase());
 		}
 
+		/// <summary>
+		/// とても小さい正数として慣習的に決めた値
+		/// ・doubleの許容誤差として
+		/// </summary>
 		public const double MICRO = 1.0 / IMAX;
 
 		private static void CheckNaN(double value)
@@ -431,7 +435,7 @@ namespace Charlotte.Commons
 			};
 		}
 
-		// memo: list を変更するので IList<T> list にはできないよ！
+		// memo: list の長さを変更するので IList<T> list にはできないよ！
 		//
 		public static T DesertElement<T>(List<T> list, int index)
 		{
@@ -447,15 +451,109 @@ namespace Charlotte.Commons
 
 		public static T FastDesertElement<T>(List<T> list, int index)
 		{
-			T ret = UnaddElement(list);
+			T ret;
 
-			if (index < list.Count)
+			if (index == list.Count - 1) // ? 終端の要素
 			{
-				T ret2 = list[index];
-				list[index] = ret;
-				ret = ret2;
+				ret = UnaddElement(list);
+			}
+			else
+			{
+				ret = list[index];
+				list[index] = UnaddElement(list);
 			}
 			return ret;
+		}
+
+		public static T RefElement<T>(IList<T> list, int index, T defval)
+		{
+			if (index < list.Count)
+			{
+				return list[index];
+			}
+			else
+			{
+				return defval;
+			}
+		}
+
+		public static IEnumerable<T> E_RemoveRange<T>(IEnumerable<T> list, int index, int count)
+		{
+			if (
+				list == null ||
+				index < 0 || list.Count() < index ||
+				count < 0 || list.Count() - index < count
+				)
+				throw new ArgumentException();
+
+			return list.Take(index).Concat(list.Skip(index + count));
+		}
+
+		public static IEnumerable<T> E_RemoveTrail<T>(IEnumerable<T> list, int count)
+		{
+			return SCommon.E_RemoveRange(list, list.Count() - count, count);
+		}
+
+		public static IEnumerable<T> E_InsertRange<T>(IEnumerable<T> list, int index, IEnumerable<T> listForInsert)
+		{
+			if (
+				list == null ||
+				listForInsert == null ||
+				index < 0 || list.Count() < index
+				)
+				throw new ArgumentException();
+
+			return list.Take(index).Concat(listForInsert).Concat(list.Skip(index));
+		}
+
+		public static IEnumerable<T> E_AddRange<T>(IEnumerable<T> list, IEnumerable<T> listForAdd)
+		{
+			return SCommon.E_InsertRange(list, list.Count(), listForAdd);
+		}
+
+		public static T[] A_RemoveRange<T>(T[] arr, int index, int count)
+		{
+			if (
+				arr == null ||
+				index < 0 || arr.Length < index ||
+				count < 0 || arr.Length - index < count
+				)
+				throw new ArgumentException();
+
+			T[] dest = new T[arr.Length - count];
+
+			Array.Copy(arr, 0, dest, 0, index);
+			Array.Copy(arr, index + count, dest, index, arr.Length - (index + count));
+
+			return dest;
+		}
+
+		public static T[] A_RemoveTrail<T>(T[] arr, int count)
+		{
+			return SCommon.A_RemoveRange(arr, arr.Length - count, count);
+		}
+
+		public static T[] A_InsertRange<T>(T[] arr, int index, T[] arrForInsert)
+		{
+			if (
+				arr == null ||
+				arrForInsert == null ||
+				index < 0 || arr.Length < index
+				)
+				throw new ArgumentException();
+
+			T[] dest = new T[arr.Length + arrForInsert.Length];
+
+			Array.Copy(arr, 0, dest, 0, index);
+			Array.Copy(arrForInsert, 0, dest, index, arrForInsert.Length);
+			Array.Copy(arr, index, dest, index + arrForInsert.Length, arr.Length - index);
+
+			return dest;
+		}
+
+		public static T[] A_AddRange<T>(T[] arr, T[] arrForAdd)
+		{
+			return SCommon.A_InsertRange(arr, arr.Length, arrForAdd);
 		}
 
 		private const int IO_TRY_MAX = 10;
@@ -560,6 +658,22 @@ namespace Charlotte.Commons
 				File.Copy(file, Path.Combine(wDir, Path.GetFileName(file)));
 		}
 
+		public static void CopyPath(string rPath, string wPath)
+		{
+			if (Directory.Exists(rPath))
+			{
+				SCommon.CopyDir(rPath, wPath);
+			}
+			else if (File.Exists(rPath))
+			{
+				File.Copy(rPath, wPath);
+			}
+			else
+			{
+				throw new Exception("コピー元パスが存在しません。");
+			}
+		}
+
 		public static string EraseExt(string path)
 		{
 			return Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
@@ -580,7 +694,7 @@ namespace Charlotte.Commons
 			return path.Substring(oldRoot.Length);
 		}
 
-		public static string PutYen(string path)
+		private static string PutYen(string path)
 		{
 			return Put_INE(path, "\\");
 		}
@@ -757,6 +871,22 @@ namespace Charlotte.Commons
 			return ToFairRelPath(path, dirSize) == path;
 		}
 
+		public static string ToCreatablePath(string path)
+		{
+			string newPath = path;
+			int n = 1;
+
+			while (File.Exists(newPath) || Directory.Exists(newPath))
+			{
+				if (n % 100 == 0)
+					ProcMain.WriteLog("パス名の衝突回避に時間が掛かっています。" + n);
+
+				newPath = SCommon.EraseExt(path) + "_" + n + Path.GetExtension(path);
+				n++;
+			}
+			return newPath;
+		}
+
 		#region ReadPart, WritePart
 
 		public static int ReadPartInt(Stream reader)
@@ -823,7 +953,7 @@ namespace Charlotte.Commons
 		{
 			if (reader.Read(buff, offset, count) != count)
 			{
-				throw new Exception("データの途中でファイルの終端に到達しました。");
+				throw new Exception("データの途中でストリームの終端に到達しました。");
 			}
 		}
 
@@ -1166,8 +1296,13 @@ namespace Charlotte.Commons
 			}
 		}
 
+		// memo: SJIS(CP-932)の中にサロゲートペアは無い。
+		// -- なので以下は保証される。
+		// ---- SCommon.GetJChars().Length == SCommon.GetJCharCodes().Count()
+
 		/// <summary>
 		/// SJIS(CP-932)の2バイト文字を全て返す。
+		/// 戻り値の文字コード：Unicode
 		/// </summary>
 		/// <returns>SJIS(CP-932)の2バイト文字の文字列</returns>
 		public static string GetJChars()
@@ -1177,6 +1312,7 @@ namespace Charlotte.Commons
 
 		/// <summary>
 		/// SJIS(CP-932)の2バイト文字を全て返す。
+		/// 戻り値の文字コード：SJIS
 		/// </summary>
 		/// <returns>SJIS(CP-932)の2バイト文字のバイト列</returns>
 		public static IEnumerable<byte> GetJCharBytes()
@@ -1190,6 +1326,7 @@ namespace Charlotte.Commons
 
 		/// <summary>
 		/// SJIS(CP-932)の2バイト文字を全て返す。
+		/// 戻り値の文字コード：SJIS
 		/// </summary>
 		/// <returns>SJIS(CP-932)の2バイト文字の列挙</returns>
 		public static IEnumerable<UInt16> GetJCharCodes()
@@ -1412,7 +1549,8 @@ namespace Charlotte.Commons
 		{
 			return GetSHA512(writePart =>
 			{
-				SCommon.ReadToEnd(reader, (buff, offset, count) => writePart(buff, offset, count));
+				SCommon.ReadToEnd(reader, writePart);
+				//SCommon.ReadToEnd(reader, (buff, offset, count) => writePart(buff, offset, count)); // old
 			});
 		}
 
@@ -1495,10 +1633,10 @@ namespace Charlotte.Commons
 			S_GetString_SJISHalfCodeRange(0x5b, 0x60) +
 			S_GetString_SJISHalfCodeRange(0x7b, 0x7e);
 
-		public static string ASCII = DECIMAL + ALPHA + alpha + PUNCT; // == GetString_SJISHalfCodeRange(0x21, 0x7e)
+		public static string ASCII = DECIMAL + ALPHA + alpha + PUNCT; // == GetString_SJISHalfCodeRange(0x21, 0x7e) // 空白(0x20)を含まないことに注意
 		public static string KANA = S_GetString_SJISHalfCodeRange(0xa1, 0xdf);
 
-		public static string HALF = ASCII + KANA;
+		public static string HALF = ASCII + KANA; // 空白(0x20)を含まないことに注意
 
 		private static string S_GetString_SJISHalfCodeRange(int codeMin, int codeMax)
 		{
@@ -1686,11 +1824,19 @@ namespace Charlotte.Commons
 			return false;
 		}
 
-		public static bool HasSame<T>(IList<T> list, Comparison<T> comp)
+		// memo: @ 2022.10.31
+		// HasSame_Comp, HasSame を同じ名前にすると Comparision<T>, Func<T, T, bool> の型推論の失敗を誘発する。
+
+		public static bool HasSame_Comp<T>(IList<T> list, Comparison<T> comp)
+		{
+			return HasSame(list, (a, b) => comp(a, b) == 0);
+		}
+
+		public static bool HasSame<T>(IList<T> list, Func<T, T, bool> match)
 		{
 			for (int r = 1; r < list.Count; r++)
 				for (int l = 0; l < r; l++)
-					if (comp(list[l], list[r]) == 0)
+					if (match(list[l], list[r]))
 						return true;
 
 			return false;
@@ -1820,6 +1966,10 @@ namespace Charlotte.Commons
 			};
 		}
 
+		// memo: GetLimitedReader は総読み込みサイズの下限を制限しない。
+		// -- 総読み込みサイズ上限・下限ともに length としたい場合は以下のようにする。
+		// ---- SCommon.GetLimitedReader(SCommon.GetReader(reader), length)
+
 		public static Read_d GetLimitedReader(Read_d reader, long remaining)
 		{
 			return (buff, offset, count) =>
@@ -1829,16 +1979,37 @@ namespace Charlotte.Commons
 
 				count = (int)Math.Min((long)count, remaining);
 				count = reader(buff, offset, count);
-				remaining -= (long)count;
+
+				if (count <= 0) // ? これ以上読み込めない
+					remaining = 0L;
+				else
+					remaining -= (long)count;
+
+				return count;
+			};
+		}
+
+		public static Read_d GetReader(Read_d reader)
+		{
+			return (buff, offset, count) =>
+			{
+				if (reader(buff, offset, count) != count)
+				{
+					throw new Exception("データの途中でストリームの終端に到達しました。");
+				}
 				return count;
 			};
 		}
 
 		public static void Batch(IList<string> commands, string workingDir = "", StartProcessWindowStyle_e winStyle = StartProcessWindowStyle_e.INVISIBLE)
 		{
+			// Batch-名は何でも良い。
+			// 折角なので何かの時のためにタスマネから目視で発見・判別し易い名前にしておく。
+			const string BATCH_NAME = "ChocolateCupCakeRecipe.bat";
+
 			using (WorkingDir wd = new WorkingDir())
 			{
-				string batFile = wd.GetPath("a.bat");
+				string batFile = wd.GetPath(BATCH_NAME);
 
 				File.WriteAllLines(batFile, commands, ENCODING_SJIS);
 
@@ -1910,6 +2081,9 @@ namespace Charlotte.Commons
 				this.Chars = (SCommon.ALPHA + SCommon.alpha + SCommon.DECIMAL + "+/").ToArray();
 				this.CharMap = new byte[(int)char.MaxValue + 1];
 
+				for (int index = 0; index <= (int)char.MaxValue; index++)
+					this.CharMap[index] = 0xff;
+
 				for (int index = 0; index < 64; index++)
 					this.CharMap[this.Chars[index]] = (byte)index;
 
@@ -1979,29 +2153,20 @@ namespace Charlotte.Commons
 			/// <summary>
 			/// Base64を元のバイト列に変換します。
 			/// 対応フォーマット：
-			/// -- Base64 Encode -- 但し改行を含まないこと。パディング無しでも良い。
+			/// -- Base64 Encode -- パディング無しでも良い。余計な空白・改行が含まれていても良い。
 			/// -- Base64 URL Encode
 			/// </summary>
 			/// <param name="src">Base64</param>
 			/// <returns>元のバイト列</returns>
 			public byte[] Decode(string src)
 			{
-				while (src.Length % 4 != 0)
-					src += CHAR_PADDING;
-
-				int destSize = (src.Length / 4) * 3;
-
-				if (destSize != 0)
+				// パディング除去
+				// 空白・改行などの不要な文字を除去する。
 				{
-					if (src[src.Length - 2] == CHAR_PADDING)
-					{
-						destSize -= 2;
-					}
-					else if (src[src.Length - 1] == CHAR_PADDING)
-					{
-						destSize--;
-					}
+					src = new string(src.Where(v => this.CharMap[(int)v] != 0xff).ToArray());
 				}
+
+				int destSize = (int)(((long)src.Length * 3) / 4);
 				byte[] dest = new byte[destSize];
 				int writer = 0;
 				int index = 0;
@@ -2113,8 +2278,9 @@ namespace Charlotte.Commons
 					ret += m * 31;
 				}
 				else
+				{
 					ret += (m - 1) * 31;
-
+				}
 				ret += d - 1;
 				ret *= 24;
 				ret += h;
@@ -2329,9 +2495,9 @@ namespace Charlotte.Commons
 				return a.GetValueForCompare() != b.GetValueForCompare();
 			}
 
-			public override bool Equals(object other)
+			public override bool Equals(object another)
 			{
-				return other is SimpleDateTime && this == (SimpleDateTime)other;
+				return another is SimpleDateTime && this == (SimpleDateTime)another;
 			}
 
 			public override int GetHashCode()
@@ -2423,6 +2589,87 @@ namespace Charlotte.Commons
 			{
 				throw null; // never
 			}
+		}
+
+		public static Exception ToThrow(Action routine)
+		{
+			try
+			{
+				routine();
+			}
+			catch (Exception ex)
+			{
+				return ex;
+			}
+			throw new Exception("例外を投げませんでした。");
+		}
+
+		public static void ToThrowPrint(Action routine)
+		{
+			Console.WriteLine("想定された例外：" + ToThrow(routine).Message);
+		}
+
+		#region GetOutputDir
+
+		// memo:
+		// 慣習的に C:\1, C:\2, C:\3, ... C:\999 をテンポラリ・暗黙の出力先として使用している。
+		// https://github.com/stackprobe/Factory/blob/master/DevTools/zz.bat -- 定期的に全削除する運用 -- 際限なく溜まらない想定
+		// あくまで個人的な慣習なので、使用する際は注意すること。
+
+		private static string GOD_Dir;
+
+		public static string GetOutputDir()
+		{
+			if (GOD_Dir == null)
+				GOD_Dir = GetOutputDir_Main();
+
+			return GOD_Dir;
+		}
+
+		private static string GetOutputDir_Main()
+		{
+			for (int c = 1; c <= 999; c++)
+			{
+				string dir = "C:\\" + c;
+
+				if (
+					!Directory.Exists(dir) &&
+					!File.Exists(dir)
+					)
+				{
+					SCommon.CreateDir(dir);
+					return dir;
+				}
+			}
+			throw new Exception("C:\\1 ～ 999 は使用できません。");
+		}
+
+		public static void OpenOutputDir()
+		{
+			SCommon.Batch(new string[] { "START " + GetOutputDir() });
+		}
+
+		public static void OpenOutputDirIfCreated()
+		{
+			if (GOD_Dir != null)
+			{
+				OpenOutputDir();
+			}
+		}
+
+		private static int NOP_Count = 0;
+
+		public static string NextOutputPath()
+		{
+			return Path.Combine(GetOutputDir(), (++NOP_Count).ToString("D4"));
+		}
+
+		#endregion
+
+		public static void Pause()
+		{
+			Console.WriteLine("Press ENTER key.");
+			Console.ReadLine();
 		}
 	}
 }
